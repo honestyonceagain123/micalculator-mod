adb7c45dbb8f71e5bf3ac8a5cd9bf30f2f3143d5
honestyonceagain123 <narutouzumakiminato321@gmail.com>
2025-10-25 12:23:33 +0530

    Deploy build folder

---
+++ precache-manifest.4e4096ea0a666dfbb2d487bb7a1c9d1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9918b7fd1477945db83afedb472731c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e561c543543f13c7ccb9",
+    "url": "./static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "1cef61c4d3d5753faf71",
+    "url": "./static/css/main.0155351f.chunk.css"
+  },
+  {
+    "revision": "e561c543543f13c7ccb9",
+    "url": "./static/js/2.1b2b40fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.1b2b40fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cef61c4d3d5753faf71",
+    "url": "./static/js/main.1c1d3301.chunk.js"
+  },
+  {
+    "revision": "a42a3ba3f2cc4632e19a",
+    "url": "./static/js/runtime-main.bb7dcd3a.js"
+  }
+]);
